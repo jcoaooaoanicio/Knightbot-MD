7c32183aed34f700a9d8a83f0b9021e2848961c9
nico <mingoianicolo88@gmail.com>
2025-05-26 06:33:25 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'nico', // Your name
-  ownerNumber: '+393533182048', //Set your number here without + symbol, just add country code & number without any space
+  botName: "samurai",
+  botOwner: 'samurqi', // Your name
+  ownerNumber: '+639487530225', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
